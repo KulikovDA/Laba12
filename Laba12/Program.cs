83589a183397138068a0cfc9a7ab33d30d4c3a64
Arion <Arion@DESKTOP-9BU80RS>
2023-09-21 16:35:16 +0200

    Добавьте файлы проекта.

---
+++ Laba12/Program.cs
@@ -0,0 +1,8 @@
+﻿//средний уровень 5
+Console.Write("Введите p:");
+double p = double.Parse(Console.ReadLine());
+const float t = 4.1f;
+double k = Math.Sqrt(p*t);
+double x = p*Math.Pow(t,2)+Math.Sqrt(k);
+double y = Math.Pow(Math.Tan(Math.Pow(x,2)),2)+ k*t;
+Console.WriteLine($"y={y:F2}");
